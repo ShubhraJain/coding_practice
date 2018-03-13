dd46ae341ccecb53f0545d09c081c4d7000be07e
ShubhraJain <00sankalp@gmail.com>
2018-03-13 13:45:11 -0700

    Recursion in progress

---
+++ Recursion/rMap.js
@@ -0,0 +1,15 @@
+// Write a recursive version of map.
+// rMap([1,2,3], timesTwo); // [2,4,6]
+
+var rMap = function(array, callback) {
+};
+
+var arr = [
+            [true, true],
+            [false, true],
+            [false, false],
+          ]
+
+console.log(!arr[1][1]);
+console.log(arr[1][1]);
+console.log(!!arr[1][1]);
