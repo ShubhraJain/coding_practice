d73f7dea2a79ed792a6e4fb02d7ede0f1d8a16b7
ShubhraJain <00sankalp@gmail.com>
2018-03-17 15:48:26 -0700

    Implemented heap sort

--- binaryHeap.js
+++ binaryHeap.js
@@ -25,23 +25,49 @@ BinaryMinHeap.prototype.insert = function(value) {
 BinaryMinHeap.prototype.extractMin = function() {
   // swap first and last value in the heap
   this.swap(0, this.heap.length - 1);
-  this.heap.pop();
+  var min = this.heap.pop();
   var idx = 0;
   var leftChildIndex = 2 * idx + 1;
-  while (this.heap[idx] > this.heap[leftChildIndex]) {
-    this.swap(idx, leftChildIndex);
-    idx = leftChildIndex;
+  var rightChildIndex = 2 * idx + 2;
+  var swappingIndex = this.getSmallerChildIndex(leftChildIndex, rightChildIndex);
+  while (this.heap[idx] > this.heap[swappingIndex]) {
+    this.swap(idx, swappingIndex);
+    idx = swappingIndex;
     leftChildIndex = 2 * idx + 1;
+    rightChildIndex = 2 * idx + 2;
+    swappingIndex = this.getSmallerChildIndex(leftChildIndex, rightChildIndex);
+  }
+  return min;
+}
+
+BinaryMinHeap.prototype.getSmallerChildIndex = function(index1, index2) {
+  if (this.heap[index1] < this.heap[index2]) {
+    return index1;
+  } else {
+    return index2;
   }
 }
 
+BinaryMinHeap.prototype.heapSort = function(array) {
+  var temp = new BinaryMinHeap();
+  array.forEach( (elem) => {
+    temp.insert(elem);
+  });
+  for (var i = 0; i < array.length; i++) {
+    var min = temp.extractMin();
+    array[i] = min;
+  }
+  return array;
+}
+
 var binaryMinHeap = new BinaryMinHeap();
-binaryMinHeap.insert(5);
-binaryMinHeap.insert(2);
-binaryMinHeap.insert(4);
-binaryMinHeap.insert(1);
-console.log(binaryMinHeap.heap);
-binaryMinHeap.extractMin();
-console.log(binaryMinHeap.heap);
-binaryMinHeap.extractMin();
-console.log(binaryMinHeap.heap);
+// binaryMinHeap.insert(5);
+// binaryMinHeap.insert(2);
+// binaryMinHeap.insert(4);
+// binaryMinHeap.insert(1);
+// console.log(binaryMinHeap.heap);
+// binaryMinHeap.extractMin();
+// console.log(binaryMinHeap.heap);
+// binaryMinHeap.extractMin();
+// console.log(binaryMinHeap.heap);
+console.log(binaryMinHeap.heapSort([4, 2, 7, 1, 9, 10, 15, 6, 4, 22, 1, 99, 26]));
