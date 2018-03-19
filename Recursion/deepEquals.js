26eb05e36f4a157cd2e031f97a67483da41c8db5
ShubhraJain <00sankalp@gmail.com>
2018-03-18 19:20:41 -0700

    Recursion in progress

---
+++ Recursion/deepEquals.js
@@ -0,0 +1,25 @@
+// Write a function that, given two objects, returns whether or not the two
+// are deeply equivalent--meaning the structure of the two objects is the
+// same, and so is the structure of each of their corresponding descendants.
+
+// Examples:
+// deepEquals({a:1, b: {c:3}},{a:1, b: {c:3}}); // true
+// deepEquals({a:1, b: {c:5}},{a:1, b: {c:6}}); // false
+
+var deepEquals = (obj1, obj2) => {
+  if (Object.keys(obj1).length !== Object.keys(obj2).length) {
+    return false;
+  }
+  for (var key in obj1) {
+    if (typeof obj1[key] === 'object' && typeof obj2[key] === 'object') {
+      return deepEquals(obj1[key], obj2[key]);
+    }
+    if (obj1[key] !== obj2[key]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+console.log(deepEquals({a:1, b: {c:3}},{a:1, b: {c:3}}));
+console.log(deepEquals({a:1, b: {c:5}},{a:1, b: {c:6}}));
