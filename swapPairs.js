df40cf3507d9123b87747f053ae5ce0f22abe081
ShubhraJain <00sankalp@gmail.com>
2018-04-17 15:26:22 -0700

    swap every two adjacent nodes of a linked list

---
+++ swapPairs.js
@@ -0,0 +1,34 @@
+// Given a linked list, swap every two adjacent nodes and return its head.
+
+// Example:
+
+// Given 1->2->3->4, you should return the list as 2->1->4->3.
+
+var swapPairs = function(head) {
+  var curr = head;
+  if (!head || !head.next) {
+      return head;
+  }
+  if (head.next) {
+    head = head.next;
+  }
+  while (curr.next) {
+    var temp = curr;
+    if (curr.next) {
+        curr = curr.next;
+        temp.next = curr.next;
+        curr.next = temp;
+    }
+    if (curr.next.next) {
+      curr = curr.next.next;
+        if (curr.next) {
+            temp.next = curr.next;
+        } else {
+            temp.next = curr;
+        }     
+    } else {
+        curr = curr.next;
+    }
+  }
+  return head;
+};
